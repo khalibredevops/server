9c5b664d11daadb6813ab883c96f6f2454fa3ef7
mr-ssd <sida.say@khalibre.com>
2025-04-10 09:07:45 +0700

    add branding true

--- FileConverter/sources/convertermaster.js
+++ FileConverter/sources/convertermaster.js
@@ -90,7 +90,6 @@ if (cluster.isMaster) {
 
   updateLicense();
 
-  fs.watchFile(cfgLicenseFile, updateLicense);
   setInterval(updateLicense, 86400000);
 } else {
   const converter = require('./converter');
